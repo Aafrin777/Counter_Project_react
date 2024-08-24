0e7381a6d018f80364419168dfe5c4fded9cb077
Aafrin777 <159065214+Aafrin777@users.noreply.github.com>
2024-08-24 23:46:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+/*
+cd react_10
+cd project_1
+npm run dev 
+*/
+
+
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+let [counter , setCounter] = useState(0)
+  // let counter = 0
+
+  let addValue = () => {
+    console.log("clicked" ,counter);
+
+    //setCounter(counter + 1)
+      counter = counter + 1
+  setCounter(counter)
+}
+
+let removeValue = () => {
+  if(counter > 0 ){
+    counter = counter - 1
+    setCounter(counter)
+  }
+}
+
+  return (
+    <>
+   <h1>countern project</h1>
+   <button
+   onClick={addValue}>Add value</button>
+   <button onClick={removeValue}>remove value</button>
+   <h3>counter value {counter}</h3>
+    </>
+  )
+}
+
+export default App
